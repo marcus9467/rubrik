69c716fc8a13c74ff56f1a14ec71dd1f622f95e6
marcus9467 <41637724+marcus9467@users.noreply.github.com>
2022-06-16 20:08:28 -0400

    Adding in more SQL related information

--- Powershell/Get-SQLHostMapping.ps1
+++ Powershell/Get-SQLHostMapping.ps1
@@ -5,10 +5,9 @@ This script will provide a mapping of SQL DBs back to their hosts as well as pro
 
 CODE HERE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 .EXAMPLE
 
-.\Get-SQLHostMapping.ps1 -ExportToCsv
+.\Get-SQLHostMapping.ps1 
 
 Provides a mapping of SQL DBs and then prints this information to the foreground. 
 
@@ -16,6 +15,11 @@ Provides a mapping of SQL DBs and then prints this information to the foreground
 .\Get-SQLHostMapping.ps1 -ExportToCsv
 
 Provides a mapping of SQL DBs and then exports this information out to CSV. 
+
+.NOTES
+    Author  : Marcus Henderson <marcus.henderson@rubrik.com>
+    Created : June 16, 2022
+    Company : Rubrik Inc.
 
 #>
 param ([cmdletbinding()]
@@ -25,6 +29,8 @@ param ([cmdletbinding()]
 #Import-Module -Name Rubrik
 #Connect-Rubrik
 $mdate = (Get-Date).tostring("yyyyMMddHHmm")
+$clusterInfo = Invoke-RubrikRESTCall -Endpoint "cluster/me" -Method GET
+$clusterId = $clusterInfo.id
 $DB_list = Get-RubrikDatabase
 $DB_list = $DB_list | Where-Object {$_.EffectiveSLADomainName -ne "UNPROTECTED"}
 $DBCount = ($DB_list | Measure-Object).Count
@@ -46,11 +52,16 @@ foreach($db in $DB_list){
         $DB_Stats | Add-Member -NotePropertyName "Name" -NotePropertyValue $db.Name
         $DB_Stats | Add-Member -NotePropertyName "ID" -NotePropertyValue $db.id
         $DB_Stats | Add-Member -NotePropertyName "DBType" -NotePropertyValue "AvailabilityGroup"
+        $DB_Stats | Add-Member -NotePropertyName "recoveryModel" -NotePropertyValue $db.recoveryModel
+        $DB_Stats | Add-Member -NotePropertyName "instanceName" -NotePropertyValue $db.instanceName
+        $DB_Stats | Add-Member -NotePropertyName "instanceId" -NotePropertyValue $db.instanceId
+        $DB_Stats | Add-Member -NotePropertyName "state" -NotePropertyValue $db.state
         $DB_Stats | Add-Member -NotePropertyName "Hosts" -NotePropertyValue ($AGHosts.rootName -join ",")
         $DB_Stats | Add-Member -NotePropertyName "HostIds" -NotePropertyValue ($AGHosts.rootId -join ",")
         $DB_Stats | Add-Member -NotePropertyName "EffectiveSLADomainName" -NotePropertyValue $db.effectiveSlaDomainName
         $DB_Stats | Add-Member -NotePropertyName "logBackupFrequencyInSeconds" -NotePropertyValue $db.logBackupFrequencyInSeconds
         $DB_Stats | Add-Member -NotePropertyName "logBackupRetentionHours" -NotePropertyValue $db.logBackupRetentionHours
+        $DB_Stats | Add-Member -NotePropertyName "primaryClusterId" -NotePropertyValue $db.primaryClusterId
         $AGDetails += $DB_Stats
     } 
     $WFCDetails = @()
@@ -71,11 +82,16 @@ foreach($db in $DB_list){
         $DB_Stats | Add-Member -NotePropertyName "Name" -NotePropertyValue $db.Name
         $DB_Stats | Add-Member -NotePropertyName "ID" -NotePropertyValue $db.id
         $DB_Stats | Add-Member -NotePropertyName "DBType" -NotePropertyValue "WindowsFailoverCluster"
+        $DB_Stats | Add-Member -NotePropertyName "recoveryModel" -NotePropertyValue $db.recoveryModel
+        $DB_Stats | Add-Member -NotePropertyName "instanceName" -NotePropertyValue $db.instanceName
+        $DB_Stats | Add-Member -NotePropertyName "instanceId" -NotePropertyValue $db.instanceId
+        $DB_Stats | Add-Member -NotePropertyName "state" -NotePropertyValue $db.state
         $DB_Stats | Add-Member -NotePropertyName "Hosts" -NotePropertyValue ($WFCHostList -join ",")
         $DB_Stats | Add-Member -NotePropertyName "HostIds" -NotePropertyValue ($WFHosts -join ",")
         $DB_Stats | Add-Member -NotePropertyName "EffectiveSLADomainName" -NotePropertyValue $db.effectiveSlaDomainName
         $DB_Stats | Add-Member -NotePropertyName "logBackupFrequencyInSeconds" -NotePropertyValue $db.logBackupFrequencyInSeconds
         $DB_Stats | Add-Member -NotePropertyName "logBackupRetentionHours" -NotePropertyValue $db.logBackupRetentionHours
+        $DB_Stats | Add-Member -NotePropertyName "primaryClusterId" -NotePropertyValue $db.primaryClusterId
         $WFCDetails += $DB_Stats
     } 
     $StandAloneDetails = @()
@@ -84,11 +100,16 @@ foreach($db in $DB_list){
         $DB_Stats | Add-Member -NotePropertyName "Name" -NotePropertyValue $db.Name
         $DB_Stats | Add-Member -NotePropertyName "ID" -NotePropertyValue $db.id
         $DB_Stats | Add-Member -NotePropertyName "DBType" -NotePropertyValue "StandAlone"
+        $DB_Stats | Add-Member -NotePropertyName "recoveryModel" -NotePropertyValue $db.recoveryModel
+        $DB_Stats | Add-Member -NotePropertyName "instanceName" -NotePropertyValue $db.instanceName
+        $DB_Stats | Add-Member -NotePropertyName "instanceId" -NotePropertyValue $db.instanceId
+        $DB_Stats | Add-Member -NotePropertyName "state" -NotePropertyValue $db.state
         $DB_Stats | Add-Member -NotePropertyName "Hosts" -NotePropertyValue ($db.rootProperties).rootName
         $DB_Stats | Add-Member -NotePropertyName "HostIds" -NotePropertyValue ($db.rootProperties).rootId
         $DB_Stats | Add-Member -NotePropertyName "EffectiveSLADomainName" -NotePropertyValue $db.effectiveSlaDomainName
         $DB_Stats | Add-Member -NotePropertyName "logBackupFrequencyInSeconds" -NotePropertyValue $db.logBackupFrequencyInSeconds
         $DB_Stats | Add-Member -NotePropertyName "logBackupRetentionHours" -NotePropertyValue $db.logBackupRetentionHours
+        $DB_Stats | Add-Member -NotePropertyName "primaryClusterId" -NotePropertyValue $db.primaryClusterId
         $StandAloneDetails += $DB_Stats
     }
     $DB_Info += $StandAloneDetails
@@ -97,11 +118,12 @@ foreach($db in $DB_list){
     $objectindex++
 }
 $DB_Info | Format-Table
-
 if($ExportToCSV){
-    Write-Host ("Exporting output to MSSQLInformation" +$mdate + ".csv")
-    $DB_Info | Export-Csv -NoTypeInformation ("MSSQLInformation" +$mdate + ".csv")
+    Write-Host ("Exporting CSV to SQLHostInfo_" + $clusterInfo.name + "_" + $mdate + ".csv")
+    $DB_Info | Export-Csv -NoTypeInformation ("SQLHostInfo_" + $clusterInfo.name + "_" + $mdate + ".csv") 
+
 }
+
 
 
 
