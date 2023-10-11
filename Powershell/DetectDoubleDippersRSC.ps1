b99ca7682e902f77e40bfcd92e08bd701c8d5229
Marcus Henderson <41637724+marcus9467@users.noreply.github.com>
2023-10-10 23:03:14 -0400

    Update DetectDoubleDippersRSC.ps1
    Functional Script that now outputs to CSV.

--- Powershell/DetectDoubleDippersRSC.ps1
+++ Powershell/DetectDoubleDippersRSC.ps1
@@ -4,11 +4,13 @@
 This script will detect objects that are protected multiple ways and offer suggestions to streamline and only backup the data once across the different snappables.
 
 .EXAMPLE
+./DetectDoubleDippersRSC.ps1 -serviceAccountJson C:\Temp\ServiceAccountJson.json
 
+This will generate a CSV that displays all instances of common host protection for review to ensure the same data isn't being backed up twice.
 
 .NOTES
     Author  : Marcus Henderson <marcus.henderson@rubrik.com> 
-    Created : October 09, 2023
+    Created : October 10, 2023
     Company : Rubrik Inc
 #>
 
@@ -17,11 +19,6 @@ param (
     [parameter(Mandatory=$true)]
     [string]$ServiceAccountJson
 )
-
-
-
-
-
 
 function connect-polaris {
 
@@ -1079,8 +1076,297 @@ function Get-MSSQLAGs{
   }
   Write-Output $sqlAgInfo
 }
+function Get-Fileset{
+  try{
+    $query = "query PhysicalHostListQuery(`$hostRoot: HostRoot!, `$first: Int!, `$after: String, `$sortBy: HierarchySortByField, `$sortOrder: SortOrder, `$filter: [Filter!]!, `$childFilter: [Filter!], `$isMultitenancyEnabled: Boolean = false) {
+      physicalHosts(hostRoot: `$hostRoot, filter: `$filter, first: `$first, after: `$after, sortBy: `$sortBy, sortOrder: `$sortOrder) {
+        edges {
+          cursor
+          node {
+            id
+            name
+            isArchived
+            descendantConnection(typeFilter: [LinuxFileset, WindowsFileset]) {
+              edges {
+                node {
+                  id
+                  name
+                  objectType
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+            authorizedOperations
+            cluster {
+              id
+              name
+              version
+              status
+              ...ClusterNodeConnectionFragment
+              __typename
+            }
+            ...OrganizationsColumnFragment @include(if: `$isMultitenancyEnabled)
+            primaryClusterLocation {
+              id
+              __typename
+            }
+            effectiveSlaDomain {
+              ...EffectiveSlaDomainFragment
+              __typename
+            }
+            osType
+            osName
+            connectionStatus {
+              connectivity
+              timestampMillis
+              __typename
+            }
+            isOracleHost
+            oracleUserDetails {
+              sysDbaUser
+              queryUser
+              __typename
+            }
+            ...PhysicalHostConnectionStatusColumnFragment
+            physicalChildConnection(typeFilter: [LinuxFileset, WindowsFileset], filter: `$childFilter) {
+              count
+              edges {
+                node {
+                  id
+                  name
+                  objectType
+                  slaPauseStatus
+                  effectiveSlaDomain {
+                    ...EffectiveSlaDomainFragment
+                    __typename
+                  }
+                  pendingSla {
+                    ...SLADomainFragment
+                    __typename
+                  }
+                  ...LinuxFilesetListFragment
+                  ...WindowsFilesetListFragment
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+            __typename
+          }
+          __typename
+        }
+        pageInfo {
+          endCursor
+          startCursor
+          hasNextPage
+          hasPreviousPage
+          __typename
+        }
+        __typename
+      }
+    }
+    
+    fragment OrganizationsColumnFragment on HierarchyObject {
+      allOrgs {
+        name
+        __typename
+      }
+      __typename
+    }
+    
+    fragment EffectiveSlaDomainFragment on SlaDomain {
+      id
+      name
+      ... on GlobalSlaReply {
+        isRetentionLockedSla
+        retentionLockMode
+        __typename
+      }
+      ... on ClusterSlaDomain {
+        fid
+        cluster {
+          id
+          name
+          __typename
+        }
+        isRetentionLockedSla
+        retentionLockMode
+        __typename
+      }
+      __typename
+    }
+    
+    fragment SLADomainFragment on SlaDomain {
+      id
+      name
+      ... on ClusterSlaDomain {
+        fid
+        cluster {
+          id
+          name
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }
+    
+    fragment ClusterNodeConnectionFragment on Cluster {
+      clusterNodeConnection {
+        nodes {
+          id
+          status
+          ipAddress
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }
+    
+    fragment PhysicalHostConnectionStatusColumnFragment on PhysicalHost {
+      id
+      authorizedOperations
+      connectionStatus {
+        connectivity
+        __typename
+      }
+      __typename
+    }
+    
+    fragment LinuxFilesetListFragment on LinuxFileset {
+      isRelic
+      excludes: pathExcluded
+      includes: pathIncluded
+      exceptions: pathExceptions
+      isPassThrough
+      replicatedObjects {
+        cluster {
+          id
+          name
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }
+    
+    fragment WindowsFilesetListFragment on WindowsFileset {
+      isRelic
+      excludes: pathExcluded
+      includes: pathIncluded
+      exceptions: pathExceptions
+      isPassThrough
+      replicatedObjects {
+        cluster {
+          id
+          name
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }"
+    $variables = "{
+      `"isMultitenancyEnabled`": true,
+      `"hostRoot`": `"LINUX_HOST_ROOT`",
+      `"first`": 50,
+      `"filter`": [
+        {
+          `"field`": `"IS_RELIC`",
+          `"texts`": [
+            `"false`"
+          ]
+        },
+        {
+          `"field`": `"IS_REPLICATED`",
+          `"texts`": [
+            `"false`"
+          ]
+        },
+        {
+          `"field`": `"IS_KUPR_HOST`",
+          `"texts`": [
+            `"false`"
+          ]
+        }
+      ],
+      `"sortBy`": `"NAME`",
+      `"sortOrder`": `"ASC`",
+      `"childFilter`": [
+        {
+          `"field`": `"IS_GHOST`",
+          `"texts`": [
+            `"false`"
+          ]
+        }
+      ]
+    }"
+    $JSON_BODY = @{
+      "variables" = $variables
+      "query" = $query
+    }
+    $filesetHostInfo = @()
+    $JSON_BODY = $JSON_BODY | ConvertTo-Json
+    $result = Invoke-WebRequest -Uri $POLARIS_URL -Method POST -Headers $headers -Body $JSON_BODY
+    $filesetHostInfo += (((($result.Content | convertFrom-Json).data).physicalHosts).edges).node
+    $variables = "{
+      `"isMultitenancyEnabled`": true,
+      `"hostRoot`": `"WINDOWS_HOST_ROOT`",
+      `"first`": 50,
+      `"filter`": [
+        {
+          `"field`": `"IS_RELIC`",
+          `"texts`": [
+            `"false`"
+          ]
+        },
+        {
+          `"field`": `"IS_REPLICATED`",
+          `"texts`": [
+            `"false`"
+          ]
+        },
+        {
+          `"field`": `"IS_KUPR_HOST`",
+          `"texts`": [
+            `"false`"
+          ]
+        }
+      ],
+      `"sortBy`": `"NAME`",
+      `"sortOrder`": `"ASC`",
+      `"childFilter`": [
+        {
+          `"field`": `"IS_GHOST`",
+          `"texts`": [
+            `"false`"
+          ]
+        }
+      ]
+    }"
+    $JSON_BODY = @{
+      "variables" = $variables
+      "query" = $query
+    }
+    $JSON_BODY = $JSON_BODY | ConvertTo-Json
+    $result = Invoke-WebRequest -Uri $POLARIS_URL -Method POST -Headers $headers -Body $JSON_BODY
+    $filesetHostInfo += (((($result.Content | convertFrom-Json).data).physicalHosts).edges).node
+
+
+  }
+  catch{
+    Write-Error("Error $($_)")
+  }
+  Write-Output $filesetHostInfo
+}
 $serviceAccountObj = Get-Content $ServiceAccountJson | ConvertFrom-Json
 $mdate = (Get-Date).tostring("yyyyMMddHHmm")
+$Output_directory = (Get-Location).path
+
 $polSession = connect-polaris
 $rubtok = $polSession.access_token
 $headers = @{
@@ -1092,6 +1378,117 @@ $Polaris_URL = ($serviceAccountObj.access_token_uri).replace("client_token", "gr
 $logoutUrl = ($serviceAccountObj.access_token_uri).replace("client_token", "session")
 
 
+#Get List of Hosts
+Write-Host "Gathering a list of common hosts across snappable types."
+$VMList = Get-VMwareVM
+$VMList = $VMList |Where-object {($_.effectiveSlaDomain).name -ne "UNPROTECTED"} |Where-object {($_.effectiveSlaDomain).name -ne "DO_NOT_PROTECT"}
+$VGList = Get-VolumeGroups
+$VGList = $VGList |where-object {(((($_.physicalChildConnection).edges).node).effectiveSlaDomain).name -ne "UNPROTECTED"} |Where-object {(((($_.physicalChildConnection).edges).node).effectiveSlaDomain).name -ne "DO_NOT_PROTECT"}
+$MSSQLDBList = Get-MSSQLDBs
+#Filter based on protection status
+$MSSQLDBList = $MSSQLDBList | Where-Object {(((($_.instanceDescendantConnection).edges).node).effectiveSlaDomain).name -ne "UNPROTECTED"} |Where-object {(((($_.instanceDescendantConnection).edges).node).effectiveSlaDomain).name -ne "DO_NOT_PROTECT"}
+$AGList = Get-MSSQLAGs
+$AGList = $AGList | Where-Object {(((($_.instanceDescendantConnection).edges).node).effectiveSlaDomain).name -ne "UNPROTECTED"} |Where-object {(((($_.instanceDescendantConnection).edges).node).effectiveSlaDomain).name -ne "DO_NOT_PROTECT"}
 
+$AGHostList = (($AGList.instances).logicalpath).name
+$filesetList = Get-Fileset
+$filesetList = $filesetList | Where-Object {($_.effectiveSlaDomain).name -ne "UNPROTECTED"} |Where-object {($_.effectiveSlaDomain).name -ne "DO_NOT_PROTECT"}
+
+Write-Host "Hosts gathered. Comparing results across snappable types to produce a list of multiple protection contexts."
+$doubleDippers = @()
+if(($filesetList | Measure-Object).count -ge 1){
+  Write-Host "Comparing Fileset Assignments....."
+  #Compare Filesets and SQL Hosts
+  $Fileset_SQL_DoubleDippers = (Compare-Object -ReferenceObject $filesetList -DifferenceObject $MSSQLDBList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $Fileset_SQL_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "Fileset&MSSQL"
+    $doubleDippers += $DoubleHit  
+  }
+  $Fileset_SQLAG_DoubleDippers = (Compare-Object -ReferenceObject $filesetList -DifferenceObject $AGHostList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $Fileset_SQLAG_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "Fileset&MSSQLAG"
+    $doubleDippers += $DoubleHit
+  }
+  #Compare VGs and Filesets 
+  $VG_Fileset_DoubleDippers = (Compare-Object -ReferenceObject $VGList -DifferenceObject $filesetList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $VG_Fileset_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "Fileset&VG"
+    $doubleDippers += $DoubleHit
+  }
+  #Compare VMs and Filesets
+  $Fileset_VMs_DoubleDippers = (Compare-Object -ReferenceObject $filesetList -DifferenceObject $VMList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $Fileset_VMs_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "Fileset&VM"
+    $doubleDippers += $DoubleHit
+  }
+}
+if(($VMList | Measure-Object).count -ge 1){
+  Write-Host "Comparing VM Assignments....."
+  #Compare SQL and VMware VMs
+  $SQL_VM_DoubleDippers = (Compare-Object -ReferenceObject $MSSQLDBList -DifferenceObject $VMList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $SQL_VM_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "VM&MSSQL"
+    $doubleDippers += $DoubleHit
+  }
+  $SQLAG_VM_DoubleDippers = (Compare-Object -ReferenceObject $AGHostList -DifferenceObject $VMList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $SQLAG_VM_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "VM&MSSQLAG"
+    $doubleDippers += $DoubleHit
+  }
+  #Compare VGs and VMware VMs
+  $VG_VMs_DoubleDippers = (Compare-Object -ReferenceObject $VGList -DifferenceObject $VMList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $VG_VMs_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "VG&VM"
+    $doubleDippers += $DoubleHit
+  }
+}
+if(($VGList | Measure-Object).count -ge 1){
+  Write-Host "Comparing VolumeGroup Assignments....."
+  #Compare VGs and SQL 
+  $VG_SQL_DoubleDippers = (Compare-Object -ReferenceObject $VGList -DifferenceObject $MSSQLDBList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $VG_SQL_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "VG&MSSQL"
+    $doubleDippers += $DoubleHit
+  }
+  $VG_SQLAG_DoubleDippers = (Compare-Object -ReferenceObject $VGList -DifferenceObject $AGHostList -IncludeEqual | Where-Object {$_.SideIndicator -eq "=="}).InputObject
+  forEach($doubler in $VG_SQLAG_DoubleDippers){
+    $DoubleHit = New-Object psobject
+    $DoubleHit | Add-Member -NotePropertyName "id" -NotePropertyValue $doubler.id
+    $DoubleHit | Add-Member -NotePropertyName "name" -NotePropertyValue $doubler.name
+    $DoubleHit | Add-Member -NotePropertyName "DoubleHitType" -NotePropertyValue "VG&MSSQLAG"
+    $doubleDippers += $DoubleHit
+  }
+}
+
+
+
+
+
+Write-Host ("Generating a CSV to file " + $Output_directory + "/doubleDippers" + $mdate + ".csv")
+$doubleDippers | Export-Csv -NoTypeInformation ($Output_directory + "/doubleDippers" +$mdate + ".csv")
 
 disconnect-polaris
