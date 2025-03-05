b44fda49c8e8aadcba685630f766910f337db0da
Marcus Henderson <41637724+marcus9467@users.noreply.github.com>
2025-03-05 17:30:09 -0500

    Create OnDemandVG.ps1

---
+++ Powershell/OnDemandVG.ps1
@@ -0,0 +1,109 @@
+<#
+
+.SYNOPSIS
+This script is to serve as a template for Powershell initiated MV backups with token based authentication
+
+CODE HERE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.Example
+./OndemandVG.ps1 -rubrikAddress "10.8.48.104" -ServiceAccountJson $serviceAccountJson -VG_ID "VolumeGroup:::c35db8d3-cee3-4212-9c87-554579f6d02c"
+
+This initiates a snapshot for VG VolumeGroup:::c35db8d3-cee3-4212-9c87-554579f6d02c. 
+
+.NOTES
+    Author  : Marcus Henderson <marcus.henderson@rubrik.com>
+    Created : March 05, 2025
+    Company : Rubrik Inc
+
+#>
+[cmdletbinding()]
+param (
+    [parameter(Mandatory=$true)]
+    [string]$ServiceAccountJson,
+    [parameter(Mandatory=$true)]
+    [string]$rubrikAddress,
+    [parameter(Mandatory=$true)]
+    [string]$VG_ID
+)
+
+$serviceAccountObj = Get-Content $ServiceAccountJson | ConvertFrom-Json
+
+<#
+#Setup the script to be able to ignore self signed certificate errors
+try {
+    add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@}
+
+catch 
+    {
+    Write-Output "Trust cert policy already set"
+    }
+
+    #Bypass self-signed certificate check issue
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+#>
+
+
+function Connect-RubrikCdm{
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$rubrikAddress,
+        [parameter(Mandatory=$true)]
+        [string]$serviceAccountJson
+    )
+    try{
+        $serviceAccountObj = Get-Content $ServiceAccountJson | ConvertFrom-Json
+          $connectionData = [ordered]@{
+              'serviceAccountId' = $serviceAccountObj.client_id
+              'secret' = $serviceAccountObj.client_secret
+          } | ConvertTo-Json
+          $uriString = "https://$($rubrikAddress)/api/v1/service_account/session"
+  
+      $rubrikCdm = Invoke-RestMethod -Method Post -uri $uriString -ContentType application/json -body $connectionData -SkipCertificateCheck
+    }
+    catch{
+      Write-Error("Error $($_)")
+    }
+    finally{
+      Write-Output $rubrikCdm
+    }
+  }
+
+$connectionInfo = Connect-RubrikCdm -rubrikAddress $rubrikAddress -serviceAccountJson $ServiceAccountJson
+$token = $connectionInfo.token
+#Setup Auth context in headers
+#Using basic auth just for ease, but in production this should be converted to something more secure. 
+$headers = @{ Authorization = "Bearer "+ $token }
+
+
+
+# Take VG Backup
+$request = (Invoke-WebRequest -SkipCertificateCheck -Uri ("https://" + $rubrikAddress + "/api/v1/volume_group/" + $VG_ID + "/snapshot") -Method POST -Headers $headers)
+$request.content | ConvertFrom-Json
+$href = (($request.content | ConvertFrom-Json).links).href
+
+# Loop to check status until snapshot process ends
+$endStatuses = "SUCCEEDED", "SUCCESSWITHWARNINGS", "FAILED", "CANCELED"
+$status = ""
+
+while ($endStatuses -notcontains $status) {
+    $hrefRequest = (Invoke-WebRequest -SkipCertificateCheck -Uri $href -Method GET -Headers $headers)
+    $status = ($hrefRequest.Content | ConvertFrom-Json).status
+    Write-Output "Current status: $status"
+    Start-Sleep -Seconds 30 # Wait for 30 seconds before checking again
+}
+
+Write-Output "Final status: $status"
